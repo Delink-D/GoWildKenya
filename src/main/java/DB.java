257f98afe254453b16bf844413e9c1c1a6e3b365
Delink-D <delinkdesigns@gmail.com>
2017-05-10 16:33:23 +0300

    Change of db credentials

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,6 +2,6 @@ import org.sql2o.*;
 
 public class DB {
 
-	// public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-83-25-217.compute-1.amazonaws.com:5432/d6447hu6c564d6", "aqpftopnhfelci", "409a2aad20dac31cd9776c8842ea9cd8ed53d33e72a8318e51362761166afbdc");
-	public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/go_wild_kenya", "delink", "0000");
+	public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-83-25-217.compute-1.amazonaws.com:5432/d6447hu6c564d6", "aqpftopnhfelci", "409a2aad20dac31cd9776c8842ea9cd8ed53d33e72a8318e51362761166afbdc");
+	// public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/go_wild_kenya", "delink", "0000");
 }
